a5204b64d669006c8963cb8a1452b3fe3e98815c
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-03-15 12:59:51 -0700

    started working on the routes

---
+++ routes/foods.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const knex = require('../knex.js');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/foods', (req, res) => {
+  knex('foods')
+    .orderBy('food_name')
+    .then((foods) => {
+      res.status(200).json(foods);
+  })
+  .catch((err) => {
+    console.error(err);
+    knex.destroy();
+    process.exit(1);
+  });
+});
