a1d06a3d0e9df6675f12489557acf7847fcbfd07
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-03-15 11:03:30 -0700

    finished migrations

--- migrations/20170314175857_users.js
+++ migrations/20170314175857_users.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+
+    table.string('username')
+      .notNullable()
+      .defaultTo('');
+
+    table.string('email')
+      .notNullable()
+      .unique();
+
+    table.specificType('hashed_password', 'char(60)')
+      .notNullable();
+
+    table.string('permissions')
+      .notNullable()
+      .defaultTo('');
+
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
 };
--- migrations/20170314175902_foods.js
+++ migrations/20170314175902_foods.js
@@ -5,40 +5,58 @@ exports.up = function(knex, Promise) {
     table.string('food_name')
       .notNullable()
       .defaultTo('');
+
+    table.integer('created_by')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .index();
+
     table.timestamps(true, true);
     table.boolean('jan')
       .notNullable()
       .defaultTo(false);
+
     table.boolean('feb')
       .notNullable()
       .defaultTo(false);
+
     table.boolean('mar')
       .notNullable()
       .defaultTo(false);
+
     table.boolean('apr')
       .notNullable()
       .defaultTo(false);
+
     table.boolean('may')
       .notNullable()
       .defaultTo(false);
+
     table.boolean('jun')
       .notNullable()
       .defaultTo(false);
+
     table.boolean('july')
       .notNullable()
       .defaultTo(false);
+
     table.boolean('aug')
       .notNullable()
       .defaultTo(false);
+
     table.boolean('sep')
       .notNullable()
       .defaultTo(false);
+
     table.boolean('oct')
       .notNullable()
       .defaultTo(false);
+
     table.boolean('nov')
       .notNullable()
       .defaultTo(false);
+
     table.boolean('dec')
       .notNullable()
       .defaultTo(false);
